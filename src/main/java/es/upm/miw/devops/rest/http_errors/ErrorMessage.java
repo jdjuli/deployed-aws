5b0802ba3b5133228ecd8a2ae75f4d3e5da62924
JBernal <j.bernal@upm.es>
2025-02-24 00:49:29 +0100

    version 1.0.0-SNAPSHOT

---
+++ src/main/java/es/upm/miw/devops/rest/http_errors/ErrorMessage.java
@@ -0,0 +1,20 @@
+package es.upm.miw.devops.rest.http_errors;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private final String error;
+    private final String message;
+    private final Integer code;
+
+    public ErrorMessage(Exception exception, Integer code) {
+        this.error = exception.getClass().getSimpleName();
+        this.message = exception.getMessage();
+        this.code = code;
+    }
+
+}
